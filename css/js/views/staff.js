94bbfba4163da8b4098d7f27114bbfb676977ddb
hotpitmaster070 <hotpitmaster070@gmail.com>
2026-06-14 11:38:02 +0200

    Add renderStaffView function to staff.js

---
+++ css/js/views/staff.js
@@ -0,0 +1,21 @@
+export function renderStaffView() {
+  return `
+    <button class="add-btn">+ Добавить сотрудника</button>
+    <div id="staffList">
+      <div class="staff-card" data-id="demo1">
+        <div class="staff-row">
+          <div class="avatar">M</div>
+          <div class="staff-info">
+            <div class="staff-name">Mubariz</div>
+            <div class="staff-meta">Повар • 1000₽/ч</div>
+          </div>
+          <button class="fire-btn">Уволить</button>
+          <button class="icon-btn calendar-btn">📅</button>
+          <button class="icon-btn">🗑</button>
+        </div>
+        <div class="calendar-container" style="display:none;"></div>
+        <a href="#" class="link">Открыть кабинет повара →</a>
+      </div>
+    </div>
+  `;
+}
