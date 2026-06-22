efdffc03598972d589953e515a84839a6679b63e
hotpitmaster070 <hotpitmaster070@gmail.com>
2026-06-22 18:33:18 +0200

    Fix formatting and add newline at end of file

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,4 @@
 export const SUPABASE_URL = 'https://xljogkyropyocvuuodfl.supabase.co'
 export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhsam9na3lyb3B5b2N2dXVvZGZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyODA5MjgsImV4cCI6MjA5Mzg1NjkyOH0.e7m1owNpYoqTpnGRKeEiMlTAIp0T0bAe28v6MX-MyVs'
+
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
