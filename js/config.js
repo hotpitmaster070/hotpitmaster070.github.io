f09a0e819da67d0ba38ff0902df2d6db775ba24f
hotpitmaster070 <hotpitmaster070@gmail.com>
2026-06-25 01:28:45 +0200

    Update Supabase URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = 'https://ТВОЙ-ПРОЕКТ.supabase.co'
+export const SUPABASE_URL = 'https://xljogkyropyocvuuodfl.supabase.co'
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhsam9na3lyb3B5b2N2dXVvZGZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyODA5MjgsImV4cCI6MjA5Mzg1NjkyOH0.e7m1owNpYoqTpnGRKeEiMlTAIp0T0bAe28v6MX-MyVs'
