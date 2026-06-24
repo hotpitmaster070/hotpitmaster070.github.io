2882909371a02b93de206a1d64f1b3b4b464e84c
hotpitmaster070 <hotpitmaster070@gmail.com>
2026-06-24 18:52:25 +0200

    Update Supabase client URL in api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 export const supabase = createClient(
-  'ТВОЙ_URL', // ← Supabase → Settings → API → Project URL
+  'https://xljogkyropyocvuuodfl.supabase.co', // ← Supabase → Settings → API → Project URL
   'ТВОЙ_ANON_KEY' // ← anon public
 )
