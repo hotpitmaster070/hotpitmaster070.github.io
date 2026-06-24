b4dae34fdf7238ac5b5ee85510cc51b67e0f026a
hotpitmaster070 <hotpitmaster070@gmail.com>
2026-06-24 18:43:43 +0200

    Delete js/supabase.js

--- js/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.3/+esm'
-export const supabase = createClient(
-  'https://xljogkyropyocvuuodfl.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhsam9na3lyb3B5b2N2dXVvZGZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyODA5MjgsImV4cCI6MjA5Mzg1NjkyOH0.e7m1owNpYoqTpnGRKeEiMlTAIp0T0bAe28v6MX-MyVs'
-)
